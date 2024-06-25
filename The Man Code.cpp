4af7dd2284906bdd0e80f047f2ae3b71cb759225
Yaldanda Priya <146719581+YaldandaPriya@users.noreply.github.com>
2024-06-25 11:29:54 +0530

    Add files via upload

---
+++ The Man Code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int p;
+	    cin>>p;
+	    cout<<ceil(p/2.0)<<" ";
+	    cout<<ceil(p/3.0)<<endl;
+	}
+
+}
